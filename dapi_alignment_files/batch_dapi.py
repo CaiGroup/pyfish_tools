c9705011357ab8570c514bb0db804f714da439b3
Katsuya Colon <klcolon@caltech.edu>
2022-01-21 11:22:54 -0800

    better example

--- dapi_alignment_files/batch_dapi.py
+++ dapi_alignment_files/batch_dapi.py
@@ -8,26 +8,26 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 print(f'This is task {JOB_ID}')
 
 #general path and position name
-directory = Path("/groups/CaiLab/personal/Lex/raw/112221_20kdash_3t3/segmentation")
+directory = Path("/groups/CaiLab/personal/Lex/raw/Linus_10k_cleared_080918_NIH3T3/")
 position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
 
 ##get all hybs with same position
 ##use this for background alignment
-files = directory / position_name
+ref = directory / "beads" /position_name
 
-# #use this for all hyb alignment
-# files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
-# files = [str(f) for f in files]
+#use this for all hyb alignment
+files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
+files = [str(f) for f in files]
 
 #get hyb 0 with same position
 ##use this for background alignment
-directory_align=Path("/groups/CaiLab/personal/Lex/raw/112221_20kdash_3t3/")
-ref, _, _ = find_matching_files(directory_align, 'HybCycle_0' + f'/{position_name}') 
+# directory_align=Path("/groups/CaiLab/personal/Lex/raw/112221_20kdash_3t3/")
+# ref, _, _ = find_matching_files(directory_align, 'HybCycle_0' + f'/{position_name}') 
 
 # #use this for normal alignment
 # ref, _, _ = find_matching_files(directory, 'HybCycle_0' + f'/{position_name}') 
 
-image_ref = str(ref[0])
-images_moving=str(files)
+image_ref = str(ref)
+images_moving=files
 
 dapi_alignment_parallel(image_ref,images_moving)
