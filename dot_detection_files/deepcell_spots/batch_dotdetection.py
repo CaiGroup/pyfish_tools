136a592108a0143e028264b8d271823ac345887d
Katsuya Colon <klcolon@caltech.edu>
2022-03-03 15:07:23 -0800

    general changes for job by pos

--- dot_detection_files/deepcell_spots/batch_dotdetection.py
+++ dot_detection_files/deepcell_spots/batch_dotdetection.py
@@ -7,25 +7,24 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 
 print(f'This is task {JOB_ID}')
 
-directory = Path("/groups/CaiLab/personal/Lex/raw/Linus_10k_cleared_080918_NIH3T3/notebook_pyfiles/dapi_aligned/fiducial_aligned/")
-position_name = 'MMStack_Pos{pos}.ome.tif'
+directory = Path("/groups/CaiLab/personal/Lex/raw/020822_erna_dash_tf/notebook_pyfiles/max_projected/")
+position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
 
-files, _, _ = find_matching_files(directory, f'HybCycle_{JOB_ID}' + f'/{position_name}')
+files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
 files = [str(f) for f in files]
-files = files[0:2]
 
 #This list should contain all hybcycles for a given pos
 img_list = files
 #Probability threshold to be considered a spot (parameter of deep cell)
 probability_threshold = 0.85
 #Cutoff for dot size beyond size_cutoff*std (from fitted gaussian)
-size_cutoff = 4
+size_cutoff = 3
 #Path for output folder. Code will automatically make the folder.
-output_folder = "/groups/CaiLab/personal/Lex/raw/Linus_10k_cleared_080918_NIH3T3/notebook_pyfiles/dots_detected/deepcell"
+output_folder = "/groups/CaiLab/personal/Lex/raw/020822_erna_dash_tf/notebook_pyfiles/dots_detected/deepcell"
 #If your data set is within channel encoded then set this true.
-encoded_within_channel = True
+encoded_within_channel = False
 #if performing job by hyb set this to true for file organization
-job_by_hyb = True
+job_by_hyb = False
 
 
 find_spots_all(img_list, probability_threshold, size_cutoff, output_folder, encoded_within_channel, job_by_hyb)
