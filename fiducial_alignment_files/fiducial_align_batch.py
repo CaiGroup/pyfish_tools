d9f4c0390ef2b0f0624718c3786706778743e084
Katsuya Colon <klcolon@caltech.edu>
2022-03-15 16:19:36 -0700

    added the extra argument options from previous script update

--- fiducial_alignment_files/fiducial_align_batch.py
+++ fiducial_alignment_files/fiducial_align_batch.py
@@ -9,7 +9,7 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 print(f'This is task {JOB_ID}')
 
 #general path and position name
-directory = Path("/groups/CaiLab/personal/Lex/raw/Linus_10k_cleared_080918_NIH3T3/notebook_pyfiles/dapi_aligned/")
+directory = Path("/groups/CaiLab/personal/Lex/raw/030922_11kgenes/notebook_pyfiles/dapi_aligned/")
 position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
 
 #use this for all hyb alignment
@@ -17,21 +17,23 @@ files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_nam
 files = [str(f) for f in files]
 
 #directory to beads
-ref = f"/groups/CaiLab/personal/Lex/raw/Linus_10k_cleared_080918_NIH3T3/beads/{position_name}"
+ref = f"/groups/CaiLab/personal/Lex/raw/030922_11kgenes/Fiducials/{position_name}"
 
 tiff_list = files #list of images
 ref_src = ref #reference bead images
 region_size=7 #bounding box for gaussian fitting
 min_distance=10 #distance between peaks for initial dot finding
 threshold_abs=1000 #raw intensity value the dots must be over
-num_peaks=1000 #number of dots to use
-max_dist=2 #maximum allowed distance a fiducial can be prior to alignment
+num_peaks=800 #number of dots to use
+max_dist=1 #maximum allowed distance a fiducial can be prior to alignment
 ransac_threshold=0.5 #maximum pixel distance a dot has to be after correction to be considered an inlier
 include_dapi=False #bool to include dapi channel
+use_ref_coord = True # use the reference coordinates to find moving dots 
 swapaxes=True #swap z and c axis
 cores = 32 #number of cores to use
 
 fiducial_align_parallel(tiff_list, ref_src, region_size=region_size, min_distance=min_distance, 
                         threshold_abs=threshold_abs, num_peaks=num_peaks, max_dist=max_dist,
                         ransac_threshold=ransac_threshold,
-                        include_dapi=include_dapi, swapaxes=swapaxes, cores=cores)
+                        include_dapi=include_dapi,use_ref_coord=use_ref_coord, swapaxes=swapaxes, cores=cores)
+
