e8d51a4b602d5cb9f8ebb3c218150b9fdf48d66f
Katsuya Colon <klcolon@caltech.edu>
2022-05-04 11:37:56 -0700

    file movement

--- dot_detection_files/daostarfinder/batch_dotdetection_individual.py
+++ dot_detection_files/daostarfinder/batch_dotdetection_individual.py
@@ -2,13 +2,17 @@ from daostarfinder_dotdetection import dot_detection_parallel
 from pathlib import Path
 import glob
 import os
+import sys
 from webfish_tools.util import find_matching_files
 
 JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 
+#get channel info
+channel = int(sys.argv[1])
+
 print(f'This is task {JOB_ID}')
 #path to processed images
-directory = Path("/groups/CaiLab/personal/Lex/raw/150genes3bind_040622/notebook_pyfiles/pre_processed_images/")
+directory = Path("/groups/CaiLab/personal/Lex/raw/050222_150genes4binding/notebook_pyfiles/pre_processed_images/")
 position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
 
 files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
@@ -16,6 +20,6 @@ files = [str(f) for f in files]
 
 size_cutoff = 4 # sigma cutoff for size distribution
 threshold = 0.02 # threshold to detect all dots
-channel = 1 #which channel to analyze
+channel = channel #which channel to analyze
 
 dot_detection_parallel(img_src = files,size_cutoff=size_cutoff, threshold=threshold, channel=channel)
