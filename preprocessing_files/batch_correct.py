f74158395f22e72bb0be4b4bc4c3e6574048a13d
Katsuya Colon <klcolon@caltech.edu>
2022-05-04 11:36:22 -0700

    change defaults

--- preprocessing_files/batch_correct.py
+++ preprocessing_files/batch_correct.py
@@ -10,7 +10,7 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 print(f'This is task {JOB_ID}')
 
 #paths for real image
-directory = Path("/groups/CaiLab/personal/Lex/raw/042022_40genes_smfish/")
+directory = Path("/groups/CaiLab/personal/Lex/raw/050222_150genes4binding/notebook_pyfiles/dapi_aligned/fiducial_aligned")
 position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
 
 files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
@@ -24,17 +24,17 @@ files = [str(f) for f in files]
 
 
 correction_type=Gaussian_and_Gamma_Correction #correction function 
-swapaxes=True #set to true if the image is c,z,x,y
+swapaxes=False #set to true if the image is c,z,x,y
 stack_bkgrd=None #path to background images
 z=1 # number of z's
 size=2048 #x,y size of image
 gamma = 1.0 #gamma enhancement
 kern_hpgb=7 #kernel size for 2d gaussian blurring
-sigma=20 #sigma for 2d or 1d gaussian blurring depending on argument combination
+sigma=15 #sigma for 2d or 1d gaussian blurring depending on argument combination
 rb_radius=5 #rolling ball radius
 hyb_offset=0 #this value is used to set a certain hyb as 0
 p_min=80 #minimum percentile for intensity clipping
-p_max = 99.99 #maximum percentile for intensity clipping
+p_max = 99.999 #maximum percentile for intensity clipping
 norm_int = True #bool to scale image
 rollingball=False #bool to perform rolling ball
 lowpass = True #bool to perform 2d lowpass gaussian blurring
