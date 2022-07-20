ebed24fb8af43953923bad77c12a2be7f4d3304a
Katsuya Colon <klcolon@caltech.edu>
2022-07-20 11:02:49 -0700

    add nack find matching files

--- aberr_correction_files/batch_chromatic_aberr.py
+++ aberr_correction_files/batch_chromatic_aberr.py
@@ -7,7 +7,14 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 
 print(f'This is task {JOB_ID}')
 
-files = ["/groups/CaiLab/personal/Lex/raw/Ratio_metric_repeat/notebook_pyfiles/max_projected/EEF2_ratiometric_Rlvl6_Llvl2_premixed_730,640,561,488,405_2000,200,300,400,400_Pos2.ome.tif", "/groups/CaiLab/personal/Lex/raw/Ratio_metric_repeat/notebook_pyfiles/max_projected/EEF2_ratiometric_Rlvl6_Llvl3_premixed_730,640,561,488,405_2000,200,300,400,400_Pos2.ome.tif"]
+#get all hybs for specific pos
+directory = Path("/groups/CaiLab/personal/Lex/raw/071622_crosstalk/notebook_pyfiles/max_projected/fiducial_aligned")
+position_name = f'MMStack_Pos{JOB_ID}.ome.tif'
+files, _, _ = find_matching_files(directory, 'HybCycle_{hyb}' + f'/{position_name}')
+files = [str(f) for f in files]
+
+# #this is for unique file names
+# files = ["/groups/CaiLab/personal/Lex/raw/Ratio_metric_repeat/notebook_pyfiles/max_projected/EEF2_ratiometric_Rlvl6_Llvl2_premixed_730,640,561,488,405_2000,200,300,400,400_Pos2.ome.tif", "/groups/CaiLab/personal/Lex/raw/Ratio_metric_repeat/notebook_pyfiles/max_projected/EEF2_ratiometric_Rlvl6_Llvl3_premixed_730,640,561,488,405_2000,200,300,400,400_Pos2.ome.tif"]
 
 #reference image
 ref_img = "/groups/CaiLab/personal/Lex/raw/Ratio_metric_repeat/notebook_pyfiles/max_projected/EEF2_ratiometric_Rlvl6_Llvl2_premixed_730,640,561,488,405_2000,200,300,400,400_Pos2.ome.tif"
