2296259058c9937657a5c8b4b89ec991fe911283
Katsuya Colon <klcolon@caltech.edu>
2022-11-21 14:02:05 -0800

    update

--- scripts/decoding_scripts/SVM_Feature_Radial_Decoding/svm_feature_radial_decoding_v2.py
+++ scripts/decoding_scripts/SVM_Feature_Radial_Decoding/svm_feature_radial_decoding_v2.py
@@ -91,97 +91,7 @@ class decode:
         self.score_brightness = score_brightness
         self.output_dir = output_dir
         
-    def lin_gen_dot_probabilities(dots_used_trues,dots_used_fakes, locations):
-        """
-        A linear SVM classifier to assign probability scores to dots. 
-        This model should be faster for larger datasets, but less accurate.
-        
-        Parameters
-        ----------
-        dots_used_trues: dot locations of true dots with features to train svm classifier
-        dots_used_fakes: dot locations of fake dots with features to train svm classifier
-        locations: all of the dots
-        """
-
-        #get attributes
-        trues = dots_used_trues[["flux","max intensity","size", "sharpness", "symmetry","roundness by gaussian fits"]]
-        trues["labels"] = 1
-        fakes = dots_used_fakes[["flux","max intensity","size", "sharpness", "symmetry","roundness by gaussian fits"]]
-        fakes["labels"] = -1
-        
-        #make sure number of fakes is > 100
-        assert len(fakes) >= 100, "Not enough fake dots to train."
-        
-        #downsample trues to match fakes
-        #generally there will be less fakes then trues 
-        trues = trues.iloc[:len(fakes),:]
-
-        #combine trues and fakes into one array
-        comb = pd.concat([trues,fakes]).reset_index(drop=True)
-
-        #Split dataset into training set and test set
-        # 70% training and 30% test
-        X_train, X_test, y_train, y_test = train_test_split(comb.iloc[:,0:6], 
-                                                            comb["labels"], test_size=0.3,random_state=42, 
-                                                            stratify=comb["labels"]) 
-
-        ### Train classifiers
-        start = time.time()
-
-        #z score normalize
-        scaler = StandardScaler().fit(X_train)
-        X_scaled = scaler.transform(X_train)
-        print(f"Number of features in dataset = {scaler.n_features_in_}")
-        print(f"Mean of features = {scaler.mean_}")
-        print(f"Variance of features = {scaler.var_}")
-
-        #parameters to test
-        C_range = np.logspace(-2, 10, 20)
-        param_grid = dict(C=C_range.tolist(), 
-                          loss = ["hinge","squared_hinge"],
-                          penalty = ["l1", "l2"])
-        
-        #total number of cross validations
-        total_cross_val = 10
-        
-        #total number of fits
-        total_fits = (len(C_range) * len(["hinge","squared_hinge"]) * len(["l1", "l2"])) * total_cross_val
-        
-        print(f"Performing hyperparameter optimization totalling {total_fits} fits...")
-        
-        #screen all permutation of parameters
-        grid = GridSearchCV(LinearSVC(max_iter=1e5, random_state=42),
-                            param_grid=param_grid, cv=total_cross_val,  n_jobs=-1)
-        grid.fit(X_scaled, y_train)
-
-        #which is the best
-        print("The best parameters are %s with a score of %0.2f on average" % (grid.best_params_, grid.best_score_))
-        print(f"Model building took {round((time.time()-start)/60, 2)} min and is now being applied to test set.")
-        #Calibrate estimator using the best parameters
-        clf = CalibratedClassifierCV(grid.best_estimator_, cv="prefit")
-        #refit classifier
-        clf = clf.fit(X_scaled, y_train)
-        # Model Accuracy: how often is the classifier correct with its labels
-        #scale test data using training mean and std
-        X_test_scaled = scaler.transform(X_test)
-        y_pred = clf.predict(X_test_scaled)
-        print(f"Label classification accuracy: {round(metrics.accuracy_score(y_test, y_pred), 2)}")
-        print("Generating probabilities for each dot...")
-        #obtain probabilities
-        y_proba = clf.predict_proba(X_test_scaled)
-        true_probs_only = y_proba[:,1]
-
-        #get probabilities for every dot
-        locations_features = locations[["flux","max intensity","size", "sharpness", "symmetry","roundness by gaussian fits"]]
-        locations_scaled = scaler.transform(locations_features)
-        loc_proba = clf.predict_proba(locations_scaled)
-        locations["probability on"] = loc_proba[:,1]
-        
-        print(f"Probabilities assigned, Total time = {round((time.time()-start)/60, 2)} min")
-        
-        return true_probs_only, X_test, y_test, locations
-
-
+    
     def rbf_gen_dot_probabilities(dots_used_trues,dots_used_fakes, locations):
         """
         A SVM classifier using a radial basis function kernel to assign probability scores to dots.
@@ -215,7 +125,7 @@ class decode:
         #Split dataset into training set and test set
         # 80% training and 20% test
         X_train, X_test, y_train, y_test = train_test_split(comb.iloc[:,0:6], 
-                                                            comb["labels"], test_size=0.20,random_state=42, 
+                                                            comb["labels"], test_size=0.20, 
                                                             stratify=comb["labels"])
 
         # Train classifiers
@@ -229,12 +139,12 @@ class decode:
         print(f"Variance of features = {scaler.var_}")
 
         #parameters to test
-        C_range = np.logspace(-2, 10, 12)
+        C_range = np.logspace(1, 12, 12)
         gamma_range = np.logspace(-9, 3, 12)
         param_grid = dict(gamma=gamma_range, C=C_range)
                                       
         #total number of cross validations
-        total_cross_val = 10
+        total_cross_val = 8
         
         #total number of fits
         total_fits = len(C_range) * len(gamma_range) * total_cross_val
@@ -242,7 +152,7 @@ class decode:
         print(f"Performing hyperparameter optimization totalling {total_fits} fits...")
                                       
         #screen all permutation of parameters
-        grid = GridSearchCV(SVC(kernel="rbf", max_iter=2e5, coef0=1,), 
+        grid = GridSearchCV(SVC(kernel="rbf", max_iter=2e5, coef0=1), 
                             param_grid=param_grid, cv=total_cross_val,  n_jobs=-1,)
         grid.fit(X_scaled, y_train)
 
@@ -251,7 +161,7 @@ class decode:
         print(f"Model building took {round((time.time()-start)/60, 2)} min and is now being applied to test set.")
         #generate probability model based on best params
         clf = SVC(kernel="rbf", max_iter=1e6, 
-                  random_state=42,gamma = grid.best_params_["gamma"], 
+                  coef0=1,gamma = grid.best_params_["gamma"], 
                   C=grid.best_params_["C"], probability=True)
         clf.fit(X_scaled, y_train)
         # Model Accuracy: how often is the classifier correct with its labels
