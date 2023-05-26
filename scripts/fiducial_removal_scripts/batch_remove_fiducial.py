0fabd362dc6dace886dbafc0534cbe0353c39765
Katsuya Colon <katsuyacolon96@gmail.com>
2023-05-26 14:44:19 -0700

    make life easier

--- scripts/fiducial_removal_scripts/batch_remove_fiducial.py
+++ scripts/fiducial_removal_scripts/batch_remove_fiducial.py
@@ -6,14 +6,14 @@ import os
 
 JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 
-#get paths of all dot locations for specific z and channel
-directory = glob(f"")
+#get paths of all dot locations for z and channel
+directory = glob(f"/groups/CaiLab/personal/Lex/raw/230521_10k_human_AD/pyfish_tools/output/dots_detected/Channel_All/*/locations_z_{JOB_ID}.csv")
 #sort positions to match
 key = [int(re.search('Pos(\\d+)*', str(f)).group(1)) for f in directory]
 locations_srcs = list(np.array(directory)[np.argsort(key)])
 
 #get reference paths
-directory = glob("")
+directory = glob(f"/groups/CaiLab/personal/Lex/raw/230521_10k_human_AD/pyfish_tools/output/pre_processed_images/chromatic_aberration/*)
 #sort positions to match
 key = [int(re.search('Pos(\\d+)*', str(f)).group(1)) for f in directory]
 fid_srcs = list(np.array(directory)[np.argsort(key)])
