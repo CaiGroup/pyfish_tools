cc20f0a8675b6aa01320bd4e917ce3cf3bd87ef9
Katsuya Colon <katsuyacolon96@gmail.com>
2023-06-16 16:48:19 -0700

    hyb coloc extra

--- scripts/colocalization_scripts/batch_hyb_coloc.py
+++ scripts/colocalization_scripts/batch_hyb_coloc.py
@@ -6,16 +6,14 @@ JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 print(f'This is task {JOB_ID}')
 
 #path to processed images
-img_dir = ""
-
-channel = int(JOB_ID) # which channel to analyze
-z=2 # which z layer to focus on
-threshold = 0.05 # intensity threshold 
-radii_list = [0.75,1,2] # various radii to test
-num_pos = 25 # number of pos
-hyb_list = [0,8] # two hybs that should colocalize
-num_channels = 4 # bool to flip z and channel axis
-
+img_dir      = "/groups/CaiLab/personal/Lex/raw/230608_4k_inv_5bs/pyfish_tools/output/pre_processed_images"
+channel      = int(JOB_ID) # which channel to analyze
+z            = 1 # which z layer to focus on
+threshold    = 0.2 # intensity threshold 
+radii_list   = [0.75,1,2] # various radii to test
+num_pos      = 50 # number of pos
+hyb_list     = [0,20] # two hybs that should colocalize
+num_channels = 4 # used for having correct axes
 
 coloc_parallel(img_dir = img_dir, channel = channel, z = z, threshold = threshold, 
                    radii_list = radii_list, num_pos = num_pos,
