371b72235e982d2794b561658f9eb2e417cb58d2
Katsuya Colon <katsuyacolon96@gmail.com>
2023-10-06 10:43:39 -0700

    fix within channel formatting

--- scripts/seqFISH_workflow/decoding/SVM_Feature_Radial_Decoding/feature_radial_decoding_batch_within_v3.py
+++ scripts/seqFISH_workflow/decoding/SVM_Feature_Radial_Decoding/feature_radial_decoding_batch_within_v3.py
@@ -2,63 +2,89 @@ from svm_feature_radial_decoding_v3 import decode
 import os
 import glob
 import sys
-
+JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
 #____________________________________________________________________________________________________________________________
-#pos = int(sys.argv[1]) #collect pos info. Note: This is legacy argument. We no longer really use this parameter anymore.
-
-channel                      = int(sys.argv[2]) #collect channel info from params file
-codebooks                    = ["codebook_string_647.csv", "codebook_string_561.csv", "codebook_string_488.csv"] #name of codebooks
-exp_dir                      = "230608_4k_inv_5bs" #name of experimental directory
+channel                      = int(sys.argv[1]) #collect channel info from params file
+codebooks                    = ["codebook_string_647.csv", "codebook_string_532.csv", "codebook_string_488.csv"] #name of codebooks
+exp_dir                      = "Linus_10k_cleared_080918_NIH3T3" #name of experimental directory
 user                         = "Lex" #name of user
-num_rounds                   = 4 #number of rounds
-first_radius                 = 1 #first search radii
-second_radius                = 1.25 #second search radii
-third_radius                 = 1.5 #third search radii if applicable
-diff                         = 1 #how many allowed drops in calls. Note: This is proportional to number of parity rounds.
-min_seed                     = 3 #how many times does a pseudocolor sequence must appear
-high_exp_seed                = 3 #how many times does pseudocolor sequence must appear for highly expressed genes
-total_hybs                   = 20 #number of total hybs
-probability_cutoff           = 0.25 #probability cutoff for On dots (0-1). Lower the value the less stringent.
-desired_fdr                  = 0.10 #desired FDR (0-1). Could set to None if you would like to filter yourself.
-use_svm                      = True #set to false if model is not helping and to save time.
-fiducial_removed             = True  #use noise removed
+num_rounds                   = 4     #number of rounds
+first_radius                 = 1.4   #first search radii
+second_radius                = 1.4   #second search radii
+third_radius                 = 2     #third search radii if applicable
+min_seed                     = 3     #how many times does a pseudocolor sequence must appear
+high_exp_seed                = 3     #how many times does pseudocolor sequence must appear for highly expressed genes
+total_hybs                   = 80    #number of total hybs
+probability_cutoff           = 0.20  #probability cutoff for On dots (0-1). Lower the value the less stringent.
+desired_fdr                  = 0.10  #desired FDR (0-1). Could set to None if you would like to filter yourself.
+use_svm                      = True  #set to false if model is not helping and to save time.
 parity_round                 = True  #do you have parity round
 include_undefined            = False #do you want locations of dots that didn't pass parity
-decode_high_exp_genes_first  = False  #do you want to decode highly expressed genes first
+decode_high_exp_genes_first  = True  #do you want to decode highly expressed genes first
 triple_decode                = True  #do you want to perform an additional third round of decoding
 score_brightness             = True  #do you want to use brightness for scoring 
 blank_ch                     = False #will you have a blank channel
+
+#path to dots
+locations_path = glob.glob(f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/pyfish_tools/output/dots_detected/fiducials_removed/Channel_{channel}/Pos{JOB_ID}/*")
 #____________________________________________________________________________________________________________________________
 #No need to really adjust unless you want alternative input or output paths
-JOB_ID = os.getenv('SLURM_ARRAY_TASK_ID', 0)
-#path to dots
-locations_path = glob.glob(f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/pyfish_tools/output/dots_detected/Channel_{channel}/spots_in_cells/Pos{JOB_ID}/*")
+
 #general codebook path
 codebook_path = f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/barcode_key/{codebooks[channel-1]}"
-#Where do you want to output the files
-output_dir = f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/pyfish_tools/output/decoded/final_{first_radius}{second_radius}{third_radius}_seed{min_seed}{high_exp_seed}_heg_svm_p{probability_cutoff*100}_diff{diff}_fdr{desired_fdr*100}/Channel_{channel}/Pos_{JOB_ID}"
 
-if len(locations_path) > 1:
-    for locations in locations_path:
-        decoder = decode(location_path=locations, codebook_path=codebook_path,
-                             num_rounds=num_rounds, first_radius=first_radius, 
-                             second_radius=second_radius,third_radius=third_radius,
-                             diff=diff, min_seed=min_seed, high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
-                             probability_cutoff=probability_cutoff, desired_fdr = desired_fdr, 
-                             output_dir=output_dir, include_undefined=include_undefined, 
-                             decode_high_exp_genes_first=decode_high_exp_genes_first,
-                             triple_decode=triple_decode, parity_round=parity_round, 
-                             score_brightness = score_brightness, blank_ch=blank_ch)
-        decoder.feature_radial_decoding(use_svm = use_svm )
+if parity_round:
+    for diff in range(2):
+        #Where do you want to output the files
+        output_dir = f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/pyfish_tools/output/decoded_fid_rem/final_{first_radius}{second_radius}{third_radius}_seed{min_seed}{high_exp_seed}_heg_svm_p{probability_cutoff*100}_diff{diff}_fdr{desired_fdr*100}/Channel_{channel}/Pos_{JOB_ID}"
+        if len(locations_path) > 1:
+            for locations in locations_path:
+                decoder = decode(location_path=locations, codebook_path=codebook_path,
+                                     num_rounds=num_rounds, first_radius=first_radius, 
+                                     second_radius=second_radius,third_radius=third_radius,
+                                     diff=diff, min_seed=min_seed, high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
+                                     probability_cutoff=probability_cutoff, desired_fdr = desired_fdr, 
+                                     output_dir=output_dir, include_undefined=include_undefined, 
+                                     decode_high_exp_genes_first=decode_high_exp_genes_first,
+                                     triple_decode=triple_decode, parity_round=parity_round, 
+                                     score_brightness = score_brightness, blank_ch=blank_ch)
+                decoder.feature_radial_decoding(use_svm = use_svm )
+        else:
+            decoder = decode(location_path=locations_path[0], codebook_path=codebook_path,
+                                    num_rounds=num_rounds, first_radius=first_radius,
+                                    second_radius=second_radius,
+                                    third_radius=third_radius, diff=diff, min_seed=min_seed,
+                                    high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
+                                    probability_cutoff=probability_cutoff,desired_fdr = desired_fdr,
+                                    output_dir=output_dir, include_undefined=include_undefined,
+                                    decode_high_exp_genes_first=decode_high_exp_genes_first,
+                                    triple_decode=triple_decode, parity_round=parity_round, 
+                                    score_brightness = score_brightness, blank_ch=blank_ch)
+            decoder.feature_radial_decoding(use_svm = use_svm )
 else:
-    decoder = decode(location_path=locations_path[0], codebook_path=codebook_path,
-                            num_rounds=num_rounds, first_radius=first_radius,
-                            second_radius=second_radius,
-                            third_radius=third_radius, diff=diff, min_seed=min_seed,
-                            high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
-                            probability_cutoff=probability_cutoff,desired_fdr = desired_fdr,
-                            output_dir=output_dir, include_undefined=include_undefined,
-                            decode_high_exp_genes_first=decode_high_exp_genes_first,
-                            triple_decode=triple_decode, parity_round=parity_round, 
-                            score_brightness = score_brightness, blank_ch=blank_ch)
-    decoder.feature_radial_decoding(use_svm = use_svm )
+    #Where do you want to output the files
+    output_dir = f"/groups/CaiLab/personal/{user}/raw/{exp_dir}/pyfish_tools/output/decoded/final_{first_radius}{second_radius}{third_radius}_seed{min_seed}{high_exp_seed}_heg_svm_p{probability_cutoff*100}_diff{diff}_fdr{desired_fdr*100}/Channel_{channel}/Pos_{JOB_ID}"
+    if len(locations_path) > 1:
+        for locations in locations_path:
+            decoder = decode(location_path=locations, codebook_path=codebook_path,
+                                 num_rounds=num_rounds, first_radius=first_radius, 
+                                 second_radius=second_radius,third_radius=third_radius,
+                                 diff=0, min_seed=min_seed, high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
+                                 probability_cutoff=probability_cutoff, desired_fdr = desired_fdr, 
+                                 output_dir=output_dir, include_undefined=include_undefined, 
+                                 decode_high_exp_genes_first=decode_high_exp_genes_first,
+                                 triple_decode=triple_decode, parity_round=parity_round, 
+                                 score_brightness = score_brightness, blank_ch=blank_ch)
+            decoder.feature_radial_decoding(use_svm = use_svm )
+    else:
+        decoder = decode(location_path=locations_path[0], codebook_path=codebook_path,
+                                num_rounds=num_rounds, first_radius=first_radius,
+                                second_radius=second_radius,
+                                third_radius=third_radius, diff=0, min_seed=min_seed,
+                                high_exp_seed=high_exp_seed, total_hybs=total_hybs, 
+                                probability_cutoff=probability_cutoff,desired_fdr = desired_fdr,
+                                output_dir=output_dir, include_undefined=include_undefined,
+                                decode_high_exp_genes_first=decode_high_exp_genes_first,
+                                triple_decode=triple_decode, parity_round=parity_round, 
+                                score_brightness = score_brightness, blank_ch=blank_ch)
+        decoder.feature_radial_decoding(use_svm = use_svm )
--- scripts/seqFISH_workflow/decoding/SVM_Feature_Radial_Decoding/multi_decoding_param_submission.py
+++ scripts/seqFISH_workflow/decoding/SVM_Feature_Radial_Decoding/multi_decoding_param_submission.py
@@ -4,8 +4,6 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 ###Here is a general template for multi parameter job submission for decoding
 
-# #number of pos
-# pos_list = np.arange(0,5,1)
 #number of channels
 channel_list = [1,2,3]
 #optional info to add to batch name
@@ -20,13 +18,13 @@ for channel in channel_list:
         f.write("#SBATCH --ntasks=1\n") #num tasks keep at 1
         f.write("#SBATCH --mem=50G\n") # RAM amount
         f.write("#SBATCH --cpus-per-task=12\n") #number of cpus
-        f.write("#SBATCH --time=6:00:00\n") #how much time
+        f.write("#SBATCH --time=48:00:00\n") #how much time
         f.write("#SBATCH --array=0-6\n") #position list or threshold (depends on intentions)
         f.write("pwd; hostname; date\n") #output path, hostname, date
         f.write("echo This is task $SLURM_ARRAY_TASK_ID\n") #output task id
         f.write("source ~/miniconda3/bin/activate\n") #activate source
         f.write("conda activate python3.7\n") #activate conda env
-        f.write(f"python feature_radial_decoding_batch_within.py 0 {channel}\n") #run batch.py and feed in arguments
+        f.write(f"python feature_radial_decoding_batch_within_v3.py {channel}\n") #run batch.py and feed in arguments
         f.write("date\n")
         f.close()
 
